13fda5f53ab96c1ea3e61c9ef1937155e1c2dece
Emalazcano <110199325+Emalazcano@users.noreply.github.com>
2026-04-21 17:15:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -441,7 +441,7 @@ for fila in filas:
 # Tabla resumen
 st.markdown("#### Detalle por traza")
 st.dataframe(
-    df_traza.sort_values("Consumo_Promedio", ascending=False).style.applymap(
+df_traza.sort_values("Consumo_Promedio", ascending=False).style.applymap(
         lambda v: 'background-color: #421212; color: white' if isinstance(v, float) and v > UMBRAL else '',
         subset=["Consumo_Promedio"]
     ),
